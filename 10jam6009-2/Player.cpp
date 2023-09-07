6db89d910c4b061fe0ee1ba9e41a84fc11b91551
user <user@DESKTOP-PQHUCC1>
2023-09-07 13:00:21 +0900

    mapClass stage1

--- 10jam6009-2/Player.cpp
+++ 10jam6009-2/Player.cpp
@@ -1,32 +1,32 @@
 #include "Player.h"
-#include <Novice.h>
-void Player::Initialize()
-{
-	playerPosX = 13 * KBlockSize;
-	playerPosY = 576;
-	playerR = 32;
-	playerSpeed = KBlockSize;
-
-	playerTmpX = 0;
-	playerTmpY = 0;
-	playerMapX = 0;
-	playerMapY = 0;
-
-	playerDraw = Novice::LoadTexture("./images/player.png");
-}
-
-void Player::Update()
-{
-	playerMapX = playerPosX / KBlockSize;
-	playerMapY = playerPosY / KBlockSize;
-}
-
-void Player::Draw()
-{
-	Novice::DrawSprite(playerPosX, playerPosY, playerDraw, 1, 1, 0, WHITE);
-}
-
-void Player::Move()
-{
-
-}
+//#include <Novice.h>
+//void Player::Initialize()
+//{
+//	/*playerPosX = 13 * KBlockSize;
+//	playerPosY = 576;
+//	playerR = 32;
+//	playerSpeed = KBlockSize;
+//
+//	playerTmpX = 0;
+//	playerTmpY = 0;
+//	playerMapX = 0;
+//	playerMapY = 0;
+//
+//	playerDraw = Novice::LoadTexture("./images/player.png");*/
+//}
+//
+//void Player::Update()
+//{
+//	/*playerMapX = playerPosX / KBlockSize;
+//	playerMapY = playerPosY / KBlockSize;*/
+//}
+//
+//void Player::Draw()
+//{
+//	/*Novice::DrawSprite(playerPosX, playerPosY, playerDraw, 1, 1, 0, WHITE);*/
+//}
+//
+//void Player::Move()
+//{
+//
+//}
--- 10jam6009-2/Player.h
+++ 10jam6009-2/Player.h
@@ -1,27 +1,27 @@
 #pragma once
 class Player
 {
-public:
-	void Initialize();
-
-	void Update();
-
-	void Draw();
-
-	void Move();
-
-private:
-	int playerDraw;
-
-	const int KBlockSize = 32;
-	int playerPosX;
-	int playerPosY ;
-	int playerR ;
-	int playerSpeed;
-
-	int playerTmpX;
-	int playerTmpY;
-	int playerMapX;
-	int playerMapY;
+//public:
+//	void Initialize();
+//
+//	void Update();
+//
+//	void Draw();
+//
+//	void Move();
+//
+//private:
+//	int playerDraw;
+//
+//	const int KBlockSize = 32;
+//	int playerPosX;
+//	int playerPosY ;
+//	int playerR ;
+//	int playerSpeed;
+//
+//	int playerTmpX;
+//	int playerTmpY;
+//	int playerMapX;
+//	int playerMapY;
 };
 
